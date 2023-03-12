a9daa492bb228dd5bf52deb651e93f4ff0239ab4
WhyPhy <100486824+Why-x-Phy@users.noreply.github.com>
2023-03-12 13:57:14 +0100

    decimals

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -84,7 +84,7 @@ const Stake: NextPage = () => {
                 <b>
                   {!claimableRewards
                     ? "Loading..."
-                    : ethers.utils.formatUnits(claimableRewards, 18)}
+                    : ethers.utils.formatUnits(claimableRewards, 2)}
                 </b>{" "}
                 {tokenBalance?.symbol}
               </p>
